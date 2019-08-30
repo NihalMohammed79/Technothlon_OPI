4d59d8b78d3f11f85a1298827af4f9c3852f6ab6
Mohammed Gouse Nihal <nihalmohammed79@gmail.com>
2019-08-30 18:22:06 +0530

    new files

--- Technothlon/app.js
+++ Technothlon/app.js
@@ -83,7 +83,7 @@ var clients =[];
 								socket.join((user[0].socketid - 20).toString());
 							}
 						}
-						if(data.level == "cards") {
+						if(data.level == "mugame") {
 							if(user[0].socketid<= 19) { 
 								socket.join(user[0].socketid.toString());
 							} else if(user[0].socketid == 20){
@@ -120,6 +120,16 @@ var clients =[];
 								socket.join((19).toString());
 							} else {
 								socket.join((user[0].socketid - 22).toString());
+							}
+						}
+						if(data.level == "odd") {
+							console.log(data, socket.id);
+							if(user[0].socketid %3 == 1) { 
+								socket.join(user[0].socketid.toString());
+							} else if(user[0].socketid %3 == 2){
+								socket.join((user[0].socketid - 1).toString());
+							} else {
+								socket.join((user[0].socketid - 2).toString());
 							}
 						}
 					}
@@ -165,17 +175,6 @@ var clients =[];
 				socket.broadcast.to((data.id - 20).toString()).emit("opponentSelection2", data);
 			}
 		});
-		socket.on("order", function(data){
-			if(data.id < 20) {
-				socket.broadcast.to((data.id).toString()).emit("order", data);
-			} else if(data.id == 20){
-				socket.broadcast.to((18).toString()).emit("order", data);
-			} else if(data.id == 21){
-				socket.broadcast.to((19).toString()).emit("order", data);
-			} else {
-				socket.broadcast.to((data.id - 21).toString()).emit("order", data);
-			}
-		});
 		socket.on("selection3", function(data){
 			if(data.id < 20) {
 				socket.broadcast.to((data.id).toString()).emit("selection3", data);
@@ -200,18 +199,6 @@ var clients =[];
 				socket.broadcast.to((data.id - 22).toString()).emit("selection3", data);
 			}
 		});
-		socket.on("chat", function(data){
-			io.sockets.emit("chat", data);
-		});   
-		socket.on("typing", function(data){
-			socket.broadcast.emit("typing", data);
-		});
-		socket.on("chat1", function(data){
-			io.sockets.emit("chat1", data);
-		});   
-		socket.on("typing1", function(data){
-			socket.broadcast.emit("typing1", data);
-		});
 		socket.on("number", function(data){
 			if(data.id < 20) {
 				socket.broadcast.to(data.id.toString()).emit("number", data);
@@ -223,12 +210,23 @@ var clients =[];
 				socket.broadcast.to((17).toString()).emit("number", data);
 			} else if(data.id == 23){
 				socket.broadcast.to((18).toString()).emit("number", data);
-			} else if(data.id == 24){
-				socket.broadcast.to((19).toString()).emit("number", data);
 			} else {
-				socket.broadcast.to((data.id - 24).toString()).emit("number", data);
+				socket.broadcast.to((data.id - 23).toString()).emit("number", data);
 			}
 		});
+		socket.on("order", function(data){
+			console.log(data);
+			io.to((data.id-2).toString()).emit("order1", data);
+		})
+		socket.on("oddnumber", function(data){
+			if(data.id %3 == 1) {
+				socket.broadcast.to(data.id.toString()).emit("oddnumber", data);
+			} else if(data.id %3 == 2){
+				socket.broadcast.to((data.id - 1).toString()).emit("oddnumber", data);
+			} else {
+				socket.broadcast.to((data.id - 2).toString()).emit("oddnumber", data);
+			}
+		})
         socket.on('disconnect', function (data) {
             for( var i=0, len=clients.length; i<len; ++i ){
                 var c = clients[i];
@@ -240,7 +238,7 @@ var clients =[];
         });
     });
 
-var levelNames = ['flash', 'square', 'nonogram2', 'poll','nonogram', 'light','invisible','alphabet','crack','people','digits','logic34', 'pi'];
+var levelNames = ['35', 'md', 'bridges', 'doors', 'square', 'nonogram2', 'poll','nonogram', 'light','invisible','alphabet','crack','people','digits','logic34', 'pi'];
 var skipdeds = [10, 10, 20, 13, 15, 20, 15, 15, 16, 17, 12, 10, 12];
 var noofusers = 1;
 // ==================
@@ -261,11 +259,6 @@ app.get("/level", isLoggedIn, function(req, res){
 	}
 });
 
-app.get("/doors", isLoggedIn, function(req, res){
-	var user = req.user;
-	res.render("doors.ejs", {user: user});
-});
-
 app.get("/profile", isLoggedIn, function(req, res){
 	var user = req.user;
 	res.render("profile.ejs", {user: user});
@@ -283,9 +276,14 @@ app.post("/skip", function(req, res){
 	res.redirect("/level");
 });
 
-app.get("/building", isLoggedIn, function(req, res){
+app.get("/building1", isLoggedIn, function(req, res){
 	var user = req.user;
-	res.render("25floor.ejs", {user: user});
+	res.render("25floor1.ejs", {user: user});
+});
+
+app.get("/building2", isLoggedIn, function(req, res){
+	var user = req.user;
+	res.render("25floor2.ejs", {user: user});
 });
 
 app.get("/triangle", isLoggedIn, function(req, res){
@@ -293,38 +291,29 @@ app.get("/triangle", isLoggedIn, function(req, res){
 	res.render("triangle.ejs", {user: user});
 });
 
+app.get("/mugame",isLoggedIn, function(req, res){
+	var user = req.user;
+	res.render('mugame.ejs', {user:user});
+});
+
 app.get("/pile", isLoggedIn, function(req, res){
 	var user = req.user;
 	res.render("pile.ejs", {user: user});
 });
 
-app.get("/cards", isLoggedIn, function(req, res){
+app.get("/half1", isLoggedIn, function(req, res){
 	var user = req.user;
-	res.render("cards.ejs", {user: user});
+	res.render("half1.ejs", {user: user});
 });
 
-app.get("/detective", isLoggedIn, function(req, res){
+app.get("/half2", isLoggedIn, function(req, res){
 	var user = req.user;
-	res.render("detective.ejs", {user: user});
+	res.render("half2.ejs", {user: user});
 });
 
-app.get("/chatroom", isLoggedIn, function(req, res){
+app.get("/odd1", isLoggedIn, function(req, res){
 	var user = req.user;
-	res.render("chat.ejs", {user: user});
-});
-
-app.get("/chatroom1", function(req, res){
-	res.render("chat1.ejs");
-});
-
-app.get("/oddgame", isLoggedIn, function(req, res){
-	var user = req.user;
-	res.render("odd.ejs");
-});
-
-app.get("/half", isLoggedIn, function(req, res){
-	var user = req.user;
-	res.render("half.ejs", {user: user});
+	res.render("odd1.ejs", {user: user});
 });
 
 app.get('/getPass',function(req,res) {
@@ -357,11 +346,11 @@ app.post("/light", function(req, res){
 	var user = req.user;
 	if(divId == "div3"){
 		user.currentLevel += 1;
-		user.score += 5;
+		user.score += 20;
 		user.hint1 = false;
 		user.hint2 = false;
 	} else {
-		user.score -= 5;
+		user.score -= 10;
 	}
 	user.save();
  	res.redirect("/level");
@@ -370,7 +359,7 @@ app.post("/light", function(req, res){
 app.post("/logic34", function(req, res){
 	var clickCount = req.body.counter;
 	var user = req.user;
-	var newScore = (1/(clickCount-23))*5;
+	var newScore = 625/clicks;
 	user.currentLevel += 1;
 	user.score += newScore;
 	user.hint1 = false;
@@ -384,7 +373,7 @@ app.post("/alphabet", function(req, res){
 	var user = req.user;
 	if(answer.toLowerCase() == "ha"){
 		user.currentLevel += 1;
-		user.score += 5;
+		user.score += 15;
 		user.hint1 = false;
 		user.hint2 = false;
 	} else {
@@ -401,7 +390,7 @@ app.post("/crack", function(req, res){
 	var user = req.user;
 	if(x == 0 && y== 4 && z == 2) {
 		user.currentLevel += 1;
-		user.score += 5;
+		user.score += 10;
 		user.hint1 = false;
 		user.hint2 = false;
 	} else {
@@ -416,7 +405,7 @@ app.post("/people", function(req, res){
 	var chosenPerson = req.body.chosenPerson;
 	if(chosenPerson == "person13") {
 		user.currentLevel += 1;
-		user.score += 5;
+		user.score += 15;
 		user.hint1 = false;
 		user.hint2 = false;
 	} else {
@@ -444,13 +433,13 @@ app.post("/digits", function(req, res){
 app.post("/pi", function(req, res){
 	var user = req.user;
 	var answer = req.body.answer;
-	if(answer.toLowerCase() == "pi") {
+	if(answer.toLowerCase() == "3.14159265") {
 		user.currentLevel += 1;
-		user.score += 5;
+		user.score += 50;
 		user.hint1 = false;
 		user.hint2 = false;
 	} else {
-		user.score -= 5;
+		user.score -= 1;
 	}
 	user.save();
  	res.redirect("/level");
@@ -461,11 +450,11 @@ app.post("/nonogram1", function(req, res){
 	var answer = req.body.answer;
 	if(answer == "1111011110110000001100001") {
 		user.currentLevel += 1;
-		user.score += 5;
+		user.score += 10;
 		user.hint1 = false;
 		user.hint2 = false;
 	} else {
-		user.score -= 5;
+		user.score -= 6;
 	}
 	user.save();
  	res.redirect("/level");
@@ -476,11 +465,11 @@ app.post("/nonogram2", function(req, res){
 	var answer = req.body.answer;
 	if(answer == "1111101111111100011111001000100100011100010100100111101011011111100111101100101110000001111001110111") {
 		user.currentLevel += 1;
-		user.score += 5;
+		user.score += 25;
 		user.hint1 = false;
 		user.hint2 = false;
 	} else {
-		user.score -= 5;
+		user.score -= 10;
 	}
 	user.save();
  	res.redirect("/level");
@@ -491,11 +480,11 @@ app.post("/poll", function(req, res){
 	var answer = req.body.answer;
 	if(answer == "122233123334122534111544555544") {
 		user.currentLevel += 1;
-		user.score += 5;
+		user.score += 15;
 		user.hint1 = false;
 		user.hint2 = false;
 	} else {
-		user.score -= 5;
+		user.score -= 7;
 	}
 	user.save();
  	res.redirect("/level");
@@ -521,29 +510,16 @@ app.post("/square", function(req, res){
 	res.redirect("/level");
 });
 
-app.post("/pattern", function(req, res){
-	var user = req.user;
-	var answer = req.body.pattern;
-	if(answer.toLowerCase() == "right") {
-		user.currentLevel += 1;
-		user.score += 5;
-		user.hint1 = false;
-		user.hint2 = false;
-		user.save();
-		res.redirect("/level");
-	}
-});
-
 app.post("/flash", function(req, res){
 	var user = req.user;
 	var answer = req.body.answer;
 	if(answer.toLowerCase() == "hard") {
 		user.currentLevel += 1;
-		user.score += 5;
+		user.score += 25;
 		user.hint1 = false;
 		user.hint2 = false;
 	} else {
-		user.score -= 5;
+		user.score -= 2;
 	}
 	user.save();
 	res.redirect("/level");
@@ -560,18 +536,52 @@ app.post("/doors", function(req, res){
 	if(first.toLowerCase() == "green" && second.toLowerCase() == "red" && third.toLowerCase() == "white" && fourth.toLowerCase() == "blue" && fifth.toLowerCase() == "yellow") {
 		if(door.toLowerCase() == "blue") {
 			user.currentLevel += 1;
-			user.score += 5;
-			user.hint1 = false;
-			user.hint2 = false;
-		} else {
-			user.currentLevel += 1;
-			user.score += 2.5;
+			user.score += 10;
 			user.hint1 = false;
 			user.hint2 = false;
 		}
 	} else {
 		user.score -= 5;
 	}
+	user.save();
+	res.redirect("/level");
+});
+
+app.post("/bridges", function(req, res){
+	var user = req.user;
+	var result = req.body.user;
+	if(result == "I won") {
+		user.score += 5;
+		user.hint1 = false;
+		user.hint2 = false;
+		user.currentLevel += 1;
+	} else {
+		user.currentLevel += 1;
+		user.score -= 5;
+		user.hint1 = false;
+		user.hint2 = false;
+	}
+	user.save();
+	res.redirect("/level");
+});
+
+app.post("/md", function(req, res){
+	var user = req.user;
+	var score = req.body.user;
+	user.hint1 = false;
+	user.hint2 = false;
+	user.currentLevel += 1;
+	user.save();
+	res.redirect("/level");
+});
+
+app.post("/35", function(req, res){
+	var user = req.user;
+	var result = req.body.result;
+	user.hint1 = false;
+	user.hint2 = false;
+	user.currentLevel += 1;
+	user.score += 5;
 	user.save();
 	res.redirect("/level");
 });
@@ -591,18 +601,6 @@ app.post("/building", function(req, res){
 	res.redirect("/level");
 });
 
-app.post("/cards", function(req, res){
-	var user = req.user;
-	var result = req.body.result;
-	if(result == "I won") {
-		user.score += 5;
-	} else {
-		user.score -= 5;
-	}
-	user.save();
-	res.redirect("/level");
-});
-
 app.post("/pile", function(req, res){
 	var user = req.user;
 	var result = req.body.result;
@@ -611,14 +609,6 @@ app.post("/pile", function(req, res){
 	} else {
 		user.score -= 5;
 	}
-	user.save();
-	res.redirect("/level");
-});
-
-app.post("/detective", function(req, res){
-	var user = req.user;
-	var answer = req.body.answer;
-	user.detectiveAnswer = answer;
 	user.save();
 	res.redirect("/level");
 });
@@ -643,6 +633,14 @@ app.post("/triangle", function(req, res){
 	} else {
 		user.score -= 5;
 	}
+	user.save();
+	res.redirect("/level");
+});
+
+app.post("/mugame" , function(req, res){
+	var user = req.user;
+	var result = req.body.result;
+	user.score += Number(result);
 	user.save();
 	res.redirect("/level");
 });
--- Technothlon/public/hint1lay.js
+++
@@ -1,3 +0,0 @@
-var User = require("./models/user");
-const morgan = require('morgan');
-const winston = require('./winston/config');
